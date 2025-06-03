f1cdf8ccde61147d78056ea9af3be85e496870c6
Shantanu Shinde <shinde.shantanu21@gmail.com>
2025-06-02 22:39:10 -0500

    Setup page deployement

--- next.config.ts
+++ next.config.ts
@@ -3,5 +3,12 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
 };
+const isProd = process.env.NODE_ENV === 'production';
+
+module.exports = {
+  output: 'export', // Required for static HTML export
+  basePath: isProd ? '/portfolio-website' : '',
+  assetPrefix: isProd ? '/portfolio-website/' : '',
+};
 
 export default nextConfig;
