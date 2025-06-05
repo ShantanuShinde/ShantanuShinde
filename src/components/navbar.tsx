12ee0e59b8d18cb062a12b2b52a91fce478c26df
Shantanu Shinde <shinde.shantanu21@gmail.com>
2025-06-05 13:45:02 -0500

    update navbar

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -5,7 +5,7 @@ import DropMenu from "./menu";
 
 export default function Navbar() {
     return (
-        <nav className="w-full px-6 py-4 shadow-md shadow-lime-200 text-shadow-md sticky top-0 right-0 bg-blue-900 z-50">
+        <nav className="w-full px-6 py-4 shadow-md shadow-lime-200 text-shadow-md fixed left-0 top-0 right-0 bg-blue-900 z-50">
           <div className="mx-auto flex items-center justify-between">
             <h1 className="text-2xl font-bold">Shantanu Shinde</h1>
             <ul className="hidden md:flex">
